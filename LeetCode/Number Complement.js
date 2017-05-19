81a59153a0c91984b57d7a8b6803649207c3380f
Varun <varun.varunsharma@outlook.co.nz>
2017-05-19 21:53:39 +1200

    476. Number Complement

---
+++ LeetCode/Number Complement.js
@@ -0,0 +1,9 @@
+﻿/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function (num) {
+    var bits = Math.floor(Math.log2(num)) + 1;
+    var maximum = Math.pow(2, bits) - 1;
+    return maximum - num;
+};
